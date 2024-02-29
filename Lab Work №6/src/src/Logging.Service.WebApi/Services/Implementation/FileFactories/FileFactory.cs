86c2ad2da6191f638f6fbd447bf292e0ecd3f4a4
Yusupov Ilya <iryusupov@edu.hse.ru>
2024-02-29 20:10:20 +0300

    added factory

---
+++ Lab Work №6/src/src/Logging.Service.WebApi/Services/Implementation/FileFactories/FileFactory.cs
@@ -0,0 +1,9 @@
+﻿using Logging.Server.Service.StreamData.Services.Implementation;
+
+namespace Logging.Service.WebApi.Services.Implementation.FileFactories
+{
+    public abstract class FileFactory
+    {
+        public abstract FileService CreateService(string mimeType);
+    }
+}
---
+++ Lab Work №6/src/src/Logging.Service.WebApi/Services/Implementation/FileFactories/XlsxFactory.cs
@@ -0,0 +1,11 @@
+﻿using Logging.Server.Service.StreamData.Services.Implementation;
+
+namespace Logging.Service.WebApi.Services.Implementation.FileFactories;
+
+public class XlsxFactory : FileFactory
+{
+    public override FileService CreateService(string mimeType)
+    {
+        return new XlsxFileService("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    }
+}
