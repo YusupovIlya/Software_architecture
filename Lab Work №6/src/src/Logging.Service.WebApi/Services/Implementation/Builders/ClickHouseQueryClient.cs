bc84afcc3fd4c288932f5907b4ee99f9ad40e27d
Yusupov Ilya <iryusupov@edu.hse.ru>
2024-02-29 21:47:43 +0300

    added builder

---
+++ Lab Work №6/src/src/Logging.Service.WebApi/Services/Implementation/Builders/ClickHouseQueryClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Logging.Service.WebApi.Services.Implementation.Builders
+{
+    public class ClickHouseQueryClient
+    {
+        private QueryBuilder _queryBuilder;
+
+        public ClickHouseQueryClient(QueryBuilder queryBuilder)
+        {
+
+            _queryBuilder = queryBuilder;
+        }
+
+        public string ConstructQuery() =>
+            _queryBuilder
+                .AddSelect("logs")
+                .AddWhere("\"userId\" = 12")
+                .BuildQuery();
+    }
+}
---
+++ Lab Work №6/src/src/Logging.Service.WebApi/Services/Implementation/Builders/QueryBuilder.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Logging.Service.WebApi.Services.Implementation.Builders
+{
+    public abstract class QueryBuilder
+    {
+        public abstract QueryBuilder AddSelect(string tableName);
+
+        public abstract QueryBuilder AddWhere(string condition);
+
+        public abstract string BuildQuery();
+    }
+}
